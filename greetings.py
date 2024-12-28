c503345fa53ae755723122a00dcfc69233f9a76d
Вячеслав <naichuk.slava@gmail.com>
2024-12-28 19:01:46 +0300

    26/12 2 homework

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Slava'
+title = 'Shopping'
+content = 'buy potatoes'
+status = 'not completed'
+created_date = '28.12.24'
+issue_date = '5.01.25'
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки в формате день-месяц-год:', created_date)
+print('Дата истечения заметки в формате день-месяц-год:', issue_date)
