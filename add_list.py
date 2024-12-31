296777c730c123633087ee5b1397833429484a9f
Вячеслав <naichuk.slava@gmail.com>
2024-12-31 11:42:47 +0300

    31/12

---
+++ add_list.py
@@ -0,0 +1,5 @@
+title_list = []
+title1 = title_list.append(input('Введите Заголовок:'))
+title2 = title_list.append(input('Введите второй заголовок:'))
+title3 = title_list.append(input('Введите третий заголовок:'))
+print(title_lists)
