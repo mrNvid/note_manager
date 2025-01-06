ee6ea1849d8a362a09c4087b592303ac1042ac60
Вячеслав <naichuk.slava@gmail.com>
2025-01-06 21:58:51 +0300

    06.01

--- update_status.py
+++ update_status.py
@@ -1,18 +1,22 @@
-status = "нет"
+status = "В процессе"
 print("Укажите новый статус заметки:")
 print("1.Выполнено")
 print("2.В процессе")
 print("3.Отложено")
 print("4.Ввести новый статус")
-nomber_status=input()
-while  int(nomber_status) <1 and int(nomber_status) >5:
-    print("Ошибка ввода, попробуйте ещё раз")
-if  nomber_status==1:
-   status = "Выполнено"
-elif int(nomber_status) == 2:
-    status = "В процессе"
-elif nomber_status == 3:
-    status = "Отложено"
-elif nomber_status == 4:
-    status=input("Введите новый статус:")
+while True:
+    nomber_status=input()
+    if  int(nomber_status)==1:
+        status = "Выполнено"
+        break
+    elif int(nomber_status) == 2:
+        status = "В процессе"
+        break
+    elif int(nomber_status) == 3:
+        status = "Отложено"
+        break
+    elif int(nomber_status) == 4:
+        status=input("Введите новый статус:")
+        break
+    else: print("Ошибка ввода, попробуйте ещё раз")
 print("Новый статус:",status)
