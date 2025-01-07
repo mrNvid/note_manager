a8b88b12e594d714e3fdc514fefd8a7c23a6f989
Вячеслав <naichuk.slava@gmail.com>
2025-01-07 16:59:43 +0300

    07.01 создание заметок

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,9 +1,23 @@
 notes = []
+note = {}
 print("Менеджер заметок")
 print("Вы можете добавить новую заметку:")
 while True:
-    note_1 = dict('title' == input('Введите заголовок заметки:'), 'content' = input('Введите текст заметки:'), 'status' = input('Введите статус заметки:'), 'created_date' = input('Введите дату создания заметки в формате день-месяц-год:'), 'issue_date' = input('Введите дату истечения заметки в формате день-месяц-год:'))
-
-    new_title = input("Хотите добавить ещё одну заметку? (да/нет)")
-    if new_title == "нет":
+    name = input('Введите имя:')
+    note['Имя'] = name
+    title = input('Введите заголовок заметки:')
+    note['Заголовок'] = title
+    content = input('Введите текст заметки:')
+    note['Описание'] = content
+    status = input('Введите статус заметки:')
+    note['Статус'] = status
+    created_date = input('Введите дату создания заметки в формате день-месяц-год:')
+    note['Дата создания'] = created_date
+    issue_date = input('Введите дату истечения заметки в формате день-месяц-год:')
+    note['Дедлайн'] = issue_date
+    notes.append(note)
+    new_note = input("Хотите добавить ещё одну заметку? (да/нет)")
+    if new_note == "нет":
         break
+for x, y in notes[1].items():
+    print(x,':', y)
