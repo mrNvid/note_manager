32b76d406fecb02c44ed6005a4aaf559c9023f81
Вячеслав <naichuk.slava@gmail.com>
2025-01-07 17:42:23 +0300

    07.01 оптимизация добавления в словарь

--- multiple_notes.py
+++ multiple_notes.py
@@ -4,20 +4,21 @@ print("Менеджер заметок")
 print("Вы можете добавить новую заметку:")
 while True:
     name = input('Введите имя:')
-    note['Имя'] = name
     title = input('Введите заголовок заметки:')
-    note['Заголовок'] = title
     content = input('Введите текст заметки:')
-    note['Описание'] = content
     status = input('Введите статус заметки:')
-    note['Статус'] = status
     created_date = input('Введите дату создания заметки в формате день-месяц-год:')
-    note['Дата создания'] = created_date
     issue_date = input('Введите дату истечения заметки в формате день-месяц-год:')
-    note['Дедлайн'] = issue_date
+    note.update({'Имя': name, 'Заголовок' : title, 'Описание' : content, 'Статус': status, 'Дата создания': created_date,'Дедлайн': issue_date},)
     notes.append(note)
+    note.clear()
     new_note = input("Хотите добавить ещё одну заметку? (да/нет)")
     if new_note == "нет":
         break
-for x, y in notes[1].items():
-    print(x,':', y)
+print('Все заметки:')
+а=0
+while True:
+    for x, y in notes:
+        print(x,':', y)
+        а=а+1
+        break
