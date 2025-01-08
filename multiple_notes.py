cef126cef57940449ffd2144cc56e586b658dc86
Вячеслав <naichuk.slava@gmail.com>
2025-01-09 00:23:20 +0300

    09.01 Ввод-вывод+

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,27 +1,36 @@
 from datetime import date
 
 notes = []
-note = {}
+names=[]
+title = []
+content = []
+status = []
+created_dates = []
+issue_dates = []
+values = []
 print("Менеджер заметок")
 print("Вы можете добавить новую заметку:")
 while True:
     try:
         print('Новая заметка:')
-        name = input('Введите имя:')
-        title = input('Введите заголовок заметки:')
-        content = input('Введите текст заметки:')
-        status = input('Введите статус заметки:')
+        names.append(input('Введите имя:'))
+        title.append(input('Введите заголовок заметки:'))
+        content.append(input('Введите текст заметки:'))
+        status.append(input('Введите статус заметки:'))
         created_date = input('Введите дату создания заметки в формате дд-мм-гггг:').split('-')
         day, month, year = [int(item) for item in created_date]
         created_date.reverse()
         created_date = date(year, month, day)
+        created_dates.append(created_date)
         issue_date = input('Введите дату истечения заметки в формате дд-мм-гггг:').split('-')
         day, month, year = [int(item) for item in issue_date]
         issue_date.reverse()
         issue_date = date(year, month, day)
-        note.update({'Имя': name, 'Заголовок' : title, 'Описание' : content, 'Статус': status, 'Дата создания': created_date,'Дедлайн': issue_date},)
-        notes.append(note)
-        note.clear()
+        issue_dates.append(issue_date)
+        notes = [{'Имя': names[i], 'Заголовок': title[i], 'Заметка': content[i],
+                  'Статус': status[i], 'Дата создания': created_dates[i],
+                  'Дедлайн': issue_dates[i]}
+                 for i in range(len(names))]
         new_note = input("Хотите добавить ещё одну заметку? (да/нет)")
         if new_note == "нет":
             break
@@ -31,10 +40,9 @@ while True:
 
 print('Все заметки:')
 a=0
-while True:
-    try:
-        for key, values in notes[a].items():
-            print(key,':', values)
-            a=a+1
-    except Exception as e:
-        print(e)
+for i in range(len(names)):
+    print('_____________________')
+    note = notes[a]
+    a=a+1
+    for key, values in note.items():
+        print(key,':', values)
