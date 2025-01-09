3afdb49148b527f9a825b6330fe974183750c6b5
Вячеслав <naichuk.slava@gmail.com>
2025-01-09 23:43:41 +0300

    09.01 Удаление заметок

--- multiple_notes.py
+++ multiple_notes.py
@@ -17,16 +17,28 @@ while True:
         title.append(input('Введите заголовок заметки:'))
         content.append(input('Введите текст заметки:'))
         status.append(input('Введите статус заметки:'))
-        created_date = input('Введите дату создания заметки в формате дд-мм-гггг:').split('-')
-        day, month, year = [int(item) for item in created_date]
-        created_date.reverse()
-        created_date = date(year, month, day)
-        created_dates.append(created_date)
-        issue_date = input('Введите дату истечения заметки в формате дд-мм-гггг:').split('-')
-        day, month, year = [int(item) for item in issue_date]
-        issue_date.reverse()
-        issue_date = date(year, month, day)
-        issue_dates.append(issue_date)
+        while True:
+            try:
+                created_date = input('Введите дату создания заметки в формате дд-мм-гггг:').split('-')
+                day, month, year = [int(item) for item in created_date]
+                created_date.reverse()
+                created_date = date(year, month, day)
+                created_dates.append(created_date)
+                break
+            except Exception as e:
+                print("Ошибка ввода, попробуйте ещё раз")
+                print(e)
+        while True:
+            try:
+                issue_date = input('Введите дату истечения заметки в формате дд-мм-гггг:').split('-')
+                day, month, year = [int(item) for item in issue_date]
+                issue_date.reverse()
+                issue_date = date(year, month, day)
+                issue_dates.append(issue_date)
+                break
+            except Exception as e:
+                print("Ошибка ввода, попробуйте ещё раз")
+                print(e)
         notes = [{'Имя': names[i], 'Заголовок': title[i], 'Заметка': content[i],
                   'Статус': status[i], 'Дата создания': created_dates[i],
                   'Дедлайн': issue_dates[i]}
@@ -39,7 +51,7 @@ while True:
         print(e)
 
 print('Все заметки:')
-for i in range(len('Имя')):
+for i in range(len(notes)):
     print('_____________________')
     note = notes[i]
     for key, values in note.items():
