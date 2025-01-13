91828ac527f24bb462b06621ef177d5c6688ffc9
Вячеслав <naichuk.slava@gmail.com>
2025-01-13 03:17:55 +0300

    13.01 функциюя display_notes(notes)

--- display_notes.py
+++ display_notes.py
@@ -1,7 +1,19 @@
-from datetime import date
 
-notes = [{'username': 'Андрей', 'title': 'Магазин', 'content': 'Помидоры купить', 'status': 'новый', 'issue_date': datetime.date(2025, 1, 18), 'created_date': datetime.date(2025, 1, 13)},
-         {'username': 'Полина', 'title': 'Учёба', 'content': 'Сдать зачёт', 'status': 'В процессе', 'issue_date': datetime.date(2025, 1, 20), 'created_date': datetime.date(2025, 1, 1)},
-         {'username': 'Эрик', 'title': 'Работа', 'content': 'Уволится', 'status': 'в процессе', 'issue_date': datetime.date(2025, 6, 2), 'created_date': datetime.date(2025, 1, 1)}]
+
+notes = [{'username': 'Андрей', 'title': 'Магазин', 'content': 'Помидоры купить', 'status': 'новый', 'issue_date': '18-01-2025', 'created_date': '13-01-2025'},
+         {'username': 'Полина', 'title': 'Учёба', 'content': 'Сдать зачёт', 'status': 'В процессе', 'issue_date': '10-01-2025', 'created_date': '01-01-2025'},
+         {'username': 'Эрик', 'title': 'Работа', 'content': 'Уволится', 'status': 'в процессе', 'issue_date': '01-02-2025', 'created_date': '01-01-2025'}]
 
 def display_notes(notes):
+    if len(notes) == 0:
+        print('Список заметок пуст')
+    else:
+        print('Все заметки:')
+        for i in range(len(notes)):
+            print('Заметка №',i+1)
+            note = notes[i]
+            for key, values in note.items():
+                print(key, ':', values)
+            print('_____________________')
+
+display_notes(notes)
