19c1d885679111ca44d1a1ceb15b28bdc64e32f8
Вячеслав <naichuk.slava@gmail.com>
2025-01-19 15:33:42 +0300

    19.01 э4з2

--- load_notes_from_file.py
+++ load_notes_from_file.py
@@ -1,4 +1,23 @@
 import yaml
+import os
+from fileinput import filename
 def load_notes_from_file(filename):
-    with open("config.yaml", "r") as file:
-        data = yaml.safe_load(file)
+    if os.path.isfile("filename.yaml"):
+        with open('filename.yaml', 'r', encoding='utf-8') as file:
+            notes = yaml.safe_load(file)
+        file.close()
+        if notes == None:
+            print('Список заметок пуст')
+        else:
+            print('Все заметки:')
+            for i in range(len(notes)):
+                print('Заметка №', i + 1)
+                note = notes[i]
+                for key, values in note.items():
+                    print(key, ':', values)
+                print('_____________________')
+        return notes
+    else:
+        print("Файл не существует")
+if __name__ == "__main__":
+    load_notes_from_file(filename)
