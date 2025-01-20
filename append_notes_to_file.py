0f4d197de5343e6eb68a2d1c2924ee7b5725d87b
Вячеслав <naichuk.slava@gmail.com>
2025-01-20 14:26:15 +0300

    20.01 э4з4

--- append_notes_to_file.py
+++ append_notes_to_file.py
@@ -1,23 +1,22 @@
-from fileinput import filename
 import yaml
+import os
+from fileinput import filename
 
-
-def save_notes_to_file(notes, filename):
+def append_notes_to_file(notes, filename):
     key_ru = ['Имя пользователя', 'Заголовок', 'Описание', 'Статус', 'Дедлайн', 'Дата создания']
-    with open('filename.yaml', 'w', encoding='utf-8') as file:
+    with open('filename.yaml', 'a', encoding='utf-8') as file:
         for i in range(len(notes)):
             note = notes[i]
             note = dict(zip(key_ru, list(note.values())))
             yaml.dump([note], file, allow_unicode=True, sort_keys=False)
     file.close()
+
 if __name__ == "__main__":
+
     notes = [{'username': 'Андрей', 'title': 'Магазин', 'content': 'Помидоры купить', 'status': 'новый',
               'issue_date': '18-01-2025', 'created_date': '13-01-2025'},
              {'username': 'Полина', 'title': 'Учёба', 'content': 'Сдать зачёт', 'status': 'В процессе',
               'issue_date': '10-01-2025', 'created_date': '01-01-2025'},
              {'username': 'Эрик', 'title': 'Работа', 'content': 'Уволится', 'status': 'в процессе',
               'issue_date': '01-02-2025', 'created_date': '01-01-2025'}]
-
-    save_notes_to_file(notes, filename)
-
-
+    append_notes_to_file(notes, filename)
